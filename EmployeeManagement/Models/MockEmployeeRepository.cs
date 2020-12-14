3f0b0a078d8b4365eb662644e19e5f948d33a8ea
Ibelyaev <Ibelyaev@DESKTOP>
2020-12-14 13:24:11 +0300

    Added data to DB

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
         {
-            new Employee() { Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@pragimtech.com" },
-            new Employee() { Id = 2, Name = "John", Department = Dept.IT, Email = "john@pragimtech.com" },
-            new Employee() { Id = 3, Name = "Sam", Department = Dept.IT, Email = "sam@pragimtech.com" },
+            new Employee() { Id = 1, Name = "Mary", Department = Dept.HR, Email = "mary@mail.com" },
+            new Employee() { Id = 2, Name = "John", Department = Dept.IT, Email = "john@mail.com" },
+            new Employee() { Id = 3, Name = "Sam", Department = Dept.IT, Email = "sam@mail.com" },
         };
         }
 
