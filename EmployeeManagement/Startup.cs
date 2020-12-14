26db8ccefe39e49814bd2df4694aff126f2dafe8
Ibelyaev <Ibelyaev@DESKTOP>
2020-12-14 11:23:03 +0300

    Added Sql Server

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -2,6 +2,8 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,6 +15,13 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,6 +31,8 @@ namespace EmployeeManagement
 
             //  Dependency Injection
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
