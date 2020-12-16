276644e40e732c12bb5bc2dfdb44be905f543c9b
Ibelyaev <Ibelyaev@DESKTOP>
2020-12-16 06:07:55 +0300

    Logging Exh

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        // Inject ASP.NET Core ILogger service. Specify the Controller
+        // Type as the generic parameter. This helps us identify later
+        // which class or controller has logged the exception
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            this.logger = logger;
+        }
+
         // If there is 404 status code, the route path will become Error/404
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
@@ -15,10 +26,12 @@ namespace EmployeeManagement.Controllers
             switch (statusCode)
             {
                 case 404:
-                    ViewBag.ErrorMessage =
-                       "Sorry, the resource you requested could not be found";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    ViewBag.ErrorMessage = "Sorry, the resource could not be found";
+                    // LogWarning() method logs the message under
+                    // Warning category in the log
+                    logger.LogWarning($"404 error occured. Path = " +
+                        $"{statusCodeResult.OriginalPath} and QueryString = " +
+                        $"{statusCodeResult.OriginalQueryString}");
                     break;
             }
 
@@ -31,11 +44,10 @@ namespace EmployeeManagement.Controllers
         {
             // Retrieve the exception Details
             var exceptionHandlerPathFeature =
-                    HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-
-            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
-            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
-            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            // LogError() method logs the exception under Error category in the log
+            logger.LogError($"The path {exceptionHandlerPathFeature.Path} " +
+                $"threw an exception {exceptionHandlerPathFeature.Error}");
 
             return View("Error");
         }
