8301a3325c49d8a97d24f167464b2bfa00ad2014
Ibelyaev <Ibelyaev@DESKTOP>
2020-12-16 12:45:22 +0300

    Added show/hide Login Logout

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -58,5 +58,12 @@ namespace EmployeeManagement.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
     }
 }
