aae97e0db897c427112a9fcb2d06aa8304ad453c
Ibelyaev <Ibelyaev@DESKTOP>
2020-12-17 06:27:08 +0300

    added return to url after loggin

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -71,7 +71,7 @@ namespace EmployeeManagement.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
         {
             if (ModelState.IsValid)
             {
@@ -80,7 +80,14 @@ namespace EmployeeManagement.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("index", "home");
+                    if (!string.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("index", "home");
+                    }
                 }
 
                 ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
